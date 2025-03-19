29718cd5f52c538371c9ba805a499c1633c09bf4
hoit1011 <parksm071011@gmail.com>
2025-03-19 11:57:27 +0900

    Initial Commit

---
+++ Linky/LinkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkyApp.swift
+//  Linky
+//
+//  Created by 박성민 on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
